45a8dd926c5e917428c67365e5d5237d4136faee
Fehmir <fehmir@gmail.com>
2023-10-07 01:19:27 +0200

    Adding the xUnit Testing Project; Testing the AccountNumber validation with Fact, Theory and InlineData attributes

---
+++ EmployeesApp.Tests/Validation/AccountNumberValidationTests.cs
@@ -0,0 +1,49 @@
+﻿using EmployeesApp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Tests.Validation
+{
+    public class AccountNumberValidationTests
+    {
+        private readonly AccountNumberValidation _validation;
+
+        public AccountNumberValidationTests() => _validation = new AccountNumberValidation();
+
+        [Fact]
+        public void IsValid_ValidAccountNumber_ReturnTrue()
+            => Assert.True(_validation.IsValid("123-4543234566-23"));
+
+        [Fact]
+        public void IsValid_AccountNumberFirstPartWrong_ReturnsFalse()
+            => Assert.False(_validation.IsValid("1234-3454565676-23"));
+
+        [Theory]
+        [InlineData("1234-3454565676-23")]
+        [InlineData("12-3454565676-23")]
+        public void IsValid_AccountNumberFirstPartWrong_WithInlineData_ReturnsFalse(string accountNumber)
+            => Assert.False(_validation.IsValid(accountNumber));
+
+        [Theory]
+        [InlineData("123-345456567-23")]
+        [InlineData("123-345456567633-23")]
+        public void IsValid_AccountNumberMiddlePartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-3434545656-2")]
+        [InlineData("123-3454565676-233")]
+        public void IsValid_AccountNumberLastPartWrong_ReturnsFalse(string accNumber)
+            => Assert.False(_validation.IsValid(accNumber));
+
+        [Theory]
+        [InlineData("123-345456567633=23")]
+        [InlineData("123+345456567633-23")]
+        [InlineData("123+345456567633=23")]
+        public void IsValid_InvalidDelimiters_ThrowsArgumentException(string accNumber)
+            => Assert.Throws<ArgumentException>(() => _validation.IsValid(accNumber));
+    }
+}
